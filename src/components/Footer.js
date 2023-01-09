b66b66845bee35af3591ac935965c9af41b5f889
wongpakho814 <wongpakho814@gmail.com>
2023-01-09 21:52:37 +0800

    Implemented Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <>
+      <a href="https://github.com/wongpakho814">GitHub</a>
+      <a href="https://www.linkedin.com/in/pak-ho-wong-498745181/">LinkedIn</a>
+      <a href="https://www.facebook.com/romand271">Facebook</a>
+    </>
+  );
+}
+
+export default Footer;
