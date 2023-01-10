8b622dce39600cee1349f409eee252ec31f6d5ef
wongpakho814 <wongpakho814@gmail.com>
2023-01-10 23:33:29 +0800

    Implemented Resume.js, updated some styling in header and footer

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,7 +1,15 @@
 import React from "react";
 
 function Resume() {
-  return <></>;
+  return (
+    <section id="resume">
+      <div>
+        <h2>
+          Download my resume <a href="#resume">here</a>!
+        </h2>
+      </div>
+    </section>
+  );
 }
 
 export default Resume;
