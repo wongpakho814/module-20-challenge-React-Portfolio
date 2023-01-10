9118d454eeb48484b564334d7d0e5e377b8318ba
wongpakho814 <wongpakho814@gmail.com>
2023-01-10 23:19:34 +0800

    Implemented Contact.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,9 +3,27 @@ import React from "react";
 function Footer() {
   return (
     <footer>
-      <a href="https://github.com/wongpakho814">GitHub</a>
-      <a href="https://www.linkedin.com/in/pak-ho-wong-498745181/">LinkedIn</a>
-      <a href="https://www.facebook.com/romand271">Facebook</a>
+      <a
+        href="https://github.com/wongpakho814"
+        target="_blank"
+        rel="noreferrer"
+      >
+        GitHub
+      </a>
+      <a
+        href="https://www.linkedin.com/in/pak-ho-wong-498745181/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        LinkedIn
+      </a>
+      <a
+        href="https://www.facebook.com/romand271"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Facebook
+      </a>
     </footer>
   );
 }
