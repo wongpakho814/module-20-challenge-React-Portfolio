656a4109f0b73c2e06e3a05852cc5492b8ebe1ce
wongpakho814 <wongpakho814@gmail.com>
2023-01-11 15:43:05 +0800

    Fixed styling issue that occur after adding titles to projects

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -4,42 +4,60 @@ import sampleImg from "../../images/template-img.png";
 
 function Portfolio() {
   return (
-    <section id="previous-work">
+    <section id="portfolio-container">
       <h2>Previous Work</h2>
-      <div>
-        <a
-          href="https://www.cervicalscreening.gov.hk/en/index.html"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img
-            src={cervicalscreening}
-            alt="cervical cancer screening program Hong Kong"
-            title="cervical cancer screening program Hong Kong"
-          />
-        </a>
-        <p>Cervical Cancer Screening Program Hong Kong</p>
-        <a href="#portfolio">
-          <img src={sampleImg} alt="sample img 2" title="sample img 2" />
-        </a>
-        <p>Sample project</p>
-        <a href="#portfolio">
-          <img src={sampleImg} alt="sample img 3" title="sample img 3" />
-        </a>
-        <p>Sample project</p>
-        <a href="#portfolio">
-          <img src={sampleImg} alt="sample img 4" title="sample img 4" />
-        </a>
-        <p>Sample project</p>
-        <a href="#portfolio">
-          <img src={sampleImg} alt="sample img 5" title="sample img 5" />
-        </a>
-        <p>Sample project</p>
-        <a href="#portfolio">
-          <img src={sampleImg} alt="sample img 6" title="sample img 6" />
-        </a>
-        <p>Sample project</p>
-      </div>
+      <section id="previous-work">
+        <div>
+          <a
+            href="https://www.cervicalscreening.gov.hk/en/index.html"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img
+              src={cervicalscreening}
+              alt="cervical cancer screening program Hong Kong"
+              title="cervical cancer screening program Hong Kong"
+            />
+          </a>
+          <p>Cervical Cancer Screening Program Hong Kong</p>
+          <p>Link to GitHub repo: N/A</p>
+        </div>
+        <div>
+          <a href="#portfolio">
+            <img src={sampleImg} alt="sample img 2" title="sample img 2" />
+          </a>
+          <p>Sample project</p>
+          <p>Link to GitHub repo: N/A</p>
+        </div>
+        <div>
+          <a href="#portfolio">
+            <img src={sampleImg} alt="sample img 3" title="sample img 3" />
+          </a>
+          <p>Sample project</p>
+          <p>Link to GitHub repo: N/A</p>
+        </div>
+        <div>
+          <a href="#portfolio">
+            <img src={sampleImg} alt="sample img 4" title="sample img 4" />
+          </a>
+          <p>Sample project</p>
+          <p>Link to GitHub repo: N/A</p>
+        </div>
+        <div>
+          <a href="#portfolio">
+            <img src={sampleImg} alt="sample img 5" title="sample img 5" />
+          </a>
+          <p>Sample project</p>
+          <p>Link to GitHub repo: N/A</p>
+        </div>
+        <div>
+          <a href="#portfolio">
+            <img src={sampleImg} alt="sample img 6" title="sample img 6" />
+          </a>
+          <p>Sample project</p>
+          <p>Link to GitHub repo: N/A</p>
+        </div>
+      </section>
     </section>
   );
 }
